1b83b2b333f07758fb66487a05018baaf8f96414
Gabriel Ulloa <gsulloa@uc.cl>
2018-05-31 11:01:24 -0400

    meals reducer

---
+++ src/redux/modules/meals.js
@@ -0,0 +1,13 @@
+const initialState = {
+  data: {
+    breakfast: [1, 2, 3],
+    lunch: [4, 5, 6],
+  },
+}
+
+export default function meals(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
